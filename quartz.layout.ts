28034c29f87559e60b8a2a46ff8ae10eb7392476
fwxzxh <jdemeca01@gmail.com>
2024-11-29 20:52:10 -0600

    Quartz sync: Nov 29, 2024, 8:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -14,7 +14,7 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
-// components for pages that display a single page (e.g. a single note)
+// components for pagetrues that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
